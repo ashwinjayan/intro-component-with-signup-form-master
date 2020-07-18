a26b4ef72b11d05381d2728f507e85fb97f080a0
ashwinjayan <ashwinjayanam@gmail.com>
2020-07-18 23:57:33 +0530

    added js input validation error message

--- js/script.js
+++ js/script.js
@@ -1,19 +1,17 @@
 const formBtn = document.querySelector(".btn");
 const inputs = document.querySelectorAll(".input");
-const email = document.querySelectorAll(".input[type='email']");
+const email = document.querySelector(".input[type='email']");
 
 function validateEmail(emailField) {
     var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
     return reg.test(emailField.value);
 }
 
-formBtn.addEventListener('click', function () {
-    if(!validateEmail(email)) {
-        error.classList.toggle('error');
-    }
-});
-
 formBtn.addEventListener("click", function() {
+    if(!validateEmail(email)) {
+        email.classList.add('error');
+    }
+
     inputs.forEach((x) => {
         if(x.value === '') {
             x.classList.add('error');
